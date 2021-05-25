ef247fa161737e7b447a05c9fed6b67740bbadd5
Saiem Gilani <saiem.gilani@gmail.com>
2021-05-25 18:13:53 -0400

    initial commit

---
+++ src/types/constants.ts
@@ -0,0 +1,4 @@
+export const NAME = 'SG'
+export const NAME_AND_DOMAIN = 'SG'
+export const BASE_URL = 'https://saiemgilani.github.io/wehoop'
+export const DEPLOYMENTS_URL = 'https://api.github.com/repos/saiemgilani/wehoop/deployments'
